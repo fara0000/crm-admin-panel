6b9b67bf7c5da7cbe558e47976eabb97b7652e9e
fara0000 <fexri_imanzade@mail.ru>
2021-06-03 11:53:18 +0300

    configure some build problems while deploy

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import RequestPage from '../views/requestsPage';
 import ActivesPage from "../views/activesPage";
 import KnowledgeBasePage from "../views/knowledgeBasePage";
